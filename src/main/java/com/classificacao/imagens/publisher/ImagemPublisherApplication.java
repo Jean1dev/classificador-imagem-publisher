ebd63973ecda2d09ec0c175c7d2e1822adda6f81
Jean <jeanlucafp@gmail.com>
2021-08-14 13:54:42 -0300

    setup project

---
+++ src/main/java/com/classificacao/imagens/publisher/ImagemPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.classificacao.imagens.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagemPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagemPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classificacao/imagens/publisher/ImagemPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classificacao.imagens.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagemPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
